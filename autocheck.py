2c0f82629ab376ae77a256ee19ee0f013427f2c3
kenty9x <hoahocqn@gmail.com>
2022-05-21 11:23:22 +0200

    Update autocheck.py

--- autocheck.py
+++ autocheck.py
@@ -14,10 +14,10 @@ if len(sys.argv) > 1:
                         data = json.load(payload)
                         data['name'] = row[1]
                         birthdate = row[2].split("/")
-                        data['birthDate'] = "{}. {}. {}.".format(birthdate[2], birthdate[1], birthdate[0])
-                        data['phoneNumber'] = row[3]
+                        data['bDate'] = "{}. {}. {}.".format(birthdate[2], birthdate[1], birthdate[0])
+                        data['phone'] = row[3]
                         data['email'] = row[4]
-                        data['extraData'][0]['value'] = row[5]
+                        data['Data'][0]['value'] = row[5]
                         payload.seek(0)
                         json.dump(data, payload, indent=4,ensure_ascii=False)
                         payload.truncate()
@@ -28,7 +28,7 @@ with open('header.json', 'r+', encoding='utf-8') as payload:
     data = json.load(payload)
     now = datetime.now()
     dt_string = now.strftime("%Y/%m/%dT%H:%M:%S")
-    data['timeSheet']['date'] = dt_string
+    data['timedata']['date'] = dt_string
     payload.seek(0)
     json.dump(data, payload, indent=4,ensure_ascii=False)
     payload.truncate()
